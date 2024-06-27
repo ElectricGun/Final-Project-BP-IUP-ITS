285b992425aff651338688b219bb8da236856802
ElectricGun <evandanendrapratama@gmail.com>
2024-06-27 17:47:53 +0700

    tihshteihestdasd

--- src/TileNames.java
+++ src/TileNames.java
@@ -2,6 +2,5 @@ public final class TileNames {
     public static final String snake = "Snake";
     public static final String tile  = "Tile";
     public static final String ladder = "Ladder";
-    public static final String extraTurn = "Extra Turn";
     public static final String startingTile = "Starting Tile";
 }
